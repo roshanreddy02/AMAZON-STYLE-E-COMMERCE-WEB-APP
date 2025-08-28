4f839e4c82356d62a1c0239c47f47c89557de8a2
Roshan <c4roshanreddy@gmail.com>
2025-08-28 19:18:36 +0530

    use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let productsHTML='';
 
 products.forEach((product)=>{
@@ -57,32 +59,32 @@ products.forEach((product)=>{
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-        button.addEventListener('click', () => {
-            const productId = button.dataset.productId;
-            let matchingItem;
+  .forEach((button) => {
+      button.addEventListener('click', () => {
+          const productId = button.dataset.productId;
+          let matchingItem;
 
-            cart.forEach((item) =>{
-                if(productId === item.productId){
-                    matchingItem=item;
-                }
-            });
+          cart.forEach((item) =>{
+              if(productId === item.productId){
+                  matchingItem=item;
+              }
+          });
 
-            if(matchingItem){
-                matchingItem.quantity += 1;
-            } else {
-                cart.push({
-                    productId: productId,
-                    quantity:1
-                });
-            }
+          if(matchingItem){
+              matchingItem.quantity += 1;
+          } else {
+              cart.push({
+                  productId: productId,
+                  quantity:1
+              });
+          }
 
-            let cartQuantity = 0;
-            cart.forEach((item) =>{
-                cartQuantity+= item.quantity;
-            });
+          let cartQuantity = 0;
+          cart.forEach((item) =>{
+              cartQuantity+= item.quantity;
+          });
 
-            document.querySelector('.js-cart-quantity')
-                .innerHTML = cartQuantity;
-        });
-    });
+          document.querySelector('.js-cart-quantity')
+              .innerHTML = cartQuantity;
+      });
+  });
