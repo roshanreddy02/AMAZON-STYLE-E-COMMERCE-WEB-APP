9fb4fe88dc1a21fabeb8532b057c208dd84c6ec1
Roshan <c4roshanreddy@gmail.com>
2025-09-15 05:40:42 +0530

    ALL successfull until now 18

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,8 @@
+import {loadCart} from '../data/cart.js'
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProductsFetch} from '../data/products.js';
-import {loadCart} from '../data/cart.js'
+
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
